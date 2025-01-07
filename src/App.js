0169fc4f29940752a303028351b29a1d71b40847
sejin jeong <sejin8603@naver.com>
2025-01-07 10:35:19 +0900

    qweqweqwe

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import Draggable3DModel from './Draggable3DModel';
+import React360 from "./React360";
 
 const App = () => {
     // 이미지 파일들이 들어있는 폴더 경로
-    const imagesContext = require.context('./images', false, /\.(jpg|jpeg|png|gif)$/);
-
-    // require.context()로 가져온 파일 목록을 배열로 변환
-    const images = imagesContext.keys().map(imagesContext);
 
     return (
         <div>
             <h1>차량 3D 모델 보기</h1>
-            <Draggable3DModel images={images} />
+            {/*<Draggable3DModel images={images} />*/}
+            <React360 dir="images" numImages={24} />
+
         </div>
     );
 };
